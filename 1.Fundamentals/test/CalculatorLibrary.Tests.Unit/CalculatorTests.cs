720f54c197ea4fae8e949daef79ceaadd69697af
Egorkas <egor.karas@opensoftdev.com>
2023-04-09 23:32:52 +0300

    Add basic test for 'add' and 'substract' methods

---
+++ 1.Fundamentals/test/CalculatorLibrary.Tests.Unit/CalculatorTests.cs
@@ -0,0 +1,47 @@
+﻿using CalculatorLibrary;
+using Xunit.Abstractions;
+
+namespace CalculatorLibraryTests;
+
+public class CalculatorTests
+{
+    // System under test
+    private readonly Calculator _sut = new ();
+
+    private readonly ITestOutputHelper _outputHelper;
+    
+    
+    // Setup
+    public CalculatorTests(ITestOutputHelper outputHelper)
+    {
+        _outputHelper = outputHelper;
+    }
+    
+    [Fact]
+    public void Add_ShouldAddTwoNumbers_WhenTwoNumbersAreIntegers()
+    {
+        // Arrange
+
+        // Act
+        var result = _sut.Add(5, 4);
+        
+        // Assert
+        Assert.Equal(9, result);
+        
+        _outputHelper.WriteLine("Hello from the Add test");
+    }
+    
+    [Fact]
+    public void Subtract_ShouldSubtractTwoNumbers_WhenTwoNumbersAreIntegers()
+    {
+        // Arrange
+
+        // Act
+        var result = _sut.Substract(5, 4);
+        
+        // Assert
+        Assert.Equal(1, result);
+        
+        _outputHelper.WriteLine("Hello from the Subtract test");
+    }
+}
